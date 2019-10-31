5d160a1fcc065514e02da362afa74d75934c4aa7
JulieZhang0102 <jzhang10@babson.edu>
2019-10-30 20:11:24 -0400

    Update work.py

--- work.py
+++ work.py
@@ -9,8 +9,20 @@ CONSUMER_SECRET = 'GDj7O1hbzoew3PT8RO0rjuggnzzl8PldYJrhzOagZmwqANyJ1h'
 t = Twython(CONSUMER_KEY, CONSUMER_SECRET,
    TOKEN, TOKEN_SECRET)
 
-data = t.search(q="Halloween", count=50)
+data = t.search(q="Halloween", count=100)
 
 
 for status in data['statuses']:
     print(status['text'])
+
+import pickle
+
+# Save data to a file (will be part of your data fetching script)
+
+with open('dickens_texts.pickle','w') as f:
+    pickle.dump(charles_dickens_texts,f)
+
+
+# Load data from a file (will be part of your data processing script)
+with open('dickens_texts.pickle','r') as input_file:
+    reloaded_copy_of_texts = pickle.load(input_file)
