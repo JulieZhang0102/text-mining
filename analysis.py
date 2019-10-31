669ed10a727ba2994620d5c1a77d50165ccd14f5
JulieZhang0102 <jzhang10@babson.edu>
2019-10-30 23:52:21 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -7,18 +7,46 @@ def read_file_to_list(path_to_file):
     f = open(path_to_file)
     tweet_list = []
     words = []
+    words_final = []
     for line in f:
         line.strip()
         tweet_list.append(line)
     for i in range(len(tweet_list)):
-        tweet_list[i].split()
-        for word in tweet_list[i]:
-            word.strip()
-            words.append(word)
-    return words
+        word = tweet_list[i].split()
+        words.append(word)
+    for i in words:
+        words_final += i
+
+        # for word in tweet_list[i]:
+        #     word.strip()
+        #     words.append(word)
+    return words_final
 
 tweet_word = read_file_to_list("assignment2/text-mining/Halloween_text")
-# print(tweet_word)
+print(tweet_word)
+
+def stop_word(tweet):
+    stopwords = []
+    new_tweet = []
+    with open('assignment2/text-mining/stopwords.txt') as f:
+        for line in f:
+            # line.strip("\r\n")
+            line = line.replace(' \n','')
+            stopwords.append(line)
+    # for word in stopwords:
+    #     if word in tweet:
+    #         tweet.remove(word)
+    for i in range(len(tweet)):
+        word = tweet[i].lower()
+        if word not in stopwords:
+            new_tweet.append(word)
+    return new_tweet
+    # print(stopwords)
+    # print(tweet)
+    # return tweet
+
+tweet_word_after_stopwords = stop_word(tweet_word)
+# print(tweet_word_after_stopwords)
 
 def count_word_frequency(tweets):
     dictionary = {}
@@ -26,23 +54,15 @@ def count_word_frequency(tweets):
         dictionary[letter] = 1 + dictionary.get(letter, 0)
     return dictionary
 
-# print(count_word_frequency(tweet_word))
+word_dictionary = count_word_frequency(tweet_word_after_stopwords)
 
 
-# def most_frequent(text):
-#     letters = [letter.lower() for letter in text if letter.isalpha()]
-#     dictionary = make_dict(letters)
-#     result = []
-#     for key in dictionary:
-#         result.append((dictionary[key], key))
-#     result.sort(reverse=True)
-#     for count, letter in result:
-#         print(letter, count)
+def most_frequent(dictionary):
+    result = []
+    for key, value in dictionary.items():
+        result.append((value, key))
+    result.sort()
+    result.reverse()
+    print(result)
 
-# most_frequent(text)
-
-"custom"
-"party"
-"pumpkin"
-"joker"
-"horror"
+most_frequent(word_dictionary)
