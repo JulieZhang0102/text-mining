d555230012bcaaa0b9fad1c0afcb000642c414f9
JulieZhang0102 <jzhang10@babson.edu>
2019-10-31 21:35:38 -0400

    Revert "Update text_similarity.py"
    This reverts commit 6dc7d0454918bd397a8b052ca188e5d1d48833ee.

--- text_similarity.py
+++ text_similarity.py
@@ -1,29 +1,38 @@
 import random
-import ast
 
-# Import tweets gathered as a list
-with open('Halloween_text_list', 'r') as f:
-    tweets_list = ast.literal_eval(f.read())
+tweet_list = []
 
-# Function to calculate the similarity between two sentences
+def read_file_to_list(path_to_file):
+    """
+    reads file of words, return a list of words
+    """
+    f = open(path_to_file)
+    for line in f:
+        line.strip()
+        tweet_list.append(line)
+    return tweet_list
+# print(read_file_to_list('Halloween_text'))
+
+
 def jaccard_similarity(sentence1, sentence2): 
-    '''
-    Take two sentences/strings as inputs and return similarity between two inputs
-    '''
     a = set(sentence1.split()) 
     b = set(sentence2.split())
     c = a.intersection(b)
     return float(len(c)) / (len(a) + len(b) - len(c))
 
-# Random select 2 tweets from tweets list and calculate the similarity, repeat for 1000 times and calculate the average similarity
-total = 0
-for i in range(1000):
-    i1 = random.randint(0,len(tweets_list)-1)
-    i2 = random.randint(0,len(tweets_list)-1)
-    sentence1 = tweets_list[i1]
-    sentence2 = tweets_list[i2]
-    total += jaccard_similarity(sentence1, sentence2)
-avg_similarity = total / 1000
+def avg_jaccard_similarity():
+    total = 0
+    count = 0
+    for i in range(1000):
+        read_file_to_list('Halloween_text')
+        i1 = random.randint(1,100)
+        i2 = random.randint(1,100)
+        sentence1 = tweet_list[i1]
+        sentence2 = tweet_list[i2]
+        total += jaccard_similarity(sentence1, sentence2)
+        count += 1
+    return total / count
 
-# Print out the average similarity
-print("After 1000 simulations, the average similarity between randomly chosen two tweets is ", avg_similarity)
+
+if __name__ == "__main__":
+    print("After 1000 simulations, the average similarity between randomly chosen two tweets is ", avg_jaccard_similarity())
