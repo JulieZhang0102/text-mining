c2cea2dd366c9659a5769818faaac9fe3982eaf9
JulieZhang0102 <jzhang10@babson.edu>
2019-10-31 21:15:20 -0400

    Create Sentiment_Analysis.py

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,30 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import ast
+
+# Import tweets gathered as a list
+with open('assignment2/text-mining/Halloween_text_list', 'r') as f:
+    tweets_list = ast.literal_eval(f.read())
+
+# Create lists for different sentiments
+score_list_neg = []
+score_list_neu = []
+score_list_pos = []
+score_list_compound = []
+
+# Sentiment analysis for each tweet in the list, and resulting sentiment scores are added to each sentiment list
+for i in range(len(tweets_list)):
+    sentence = tweets_list[i]
+    score = SentimentIntensityAnalyzer().polarity_scores(sentence)
+    score_list_neg.append(score['neg'])
+    score_list_neu.append(score['neu'])
+    score_list_pos.append(score['pos'])
+    score_list_compound.append(score['compound'])
+
+# Calculate the average score of each sentiment
+avg_neg = sum(score_list_neg)/100
+avg_neu = sum(score_list_neu)/100
+avg_pos = sum(score_list_pos)/100
+avg_compound = sum(score_list_compound)/100
+
+# Print out the average score for each sentiment
+print('The average negative score is {:.2f}; neutral score is {:.2f}; positive score is {:.2f}; compound score is {:.2f}.'.format(avg_neg, avg_neu,avg_pos,avg_compound))
